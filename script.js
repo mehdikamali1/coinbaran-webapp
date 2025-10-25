3f6b5b5f47d3f88ce20e5a1f4811e1c0a131de5c
mehdikamali1 <mehdi.kamali2020@gmail.com>
2025-10-25 21:59:07 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     const tg = window.Telegram.WebApp;
     
-    const API_BASE_URL = "https://mine-maker-clinton-face.trycloudflare.com"; // <-- این آدرس تونل شماست
+    const API_BASE_URL = " https://upper-catalog-wool-tub.trycloudflare.com"; // <-- این آدرس تونل شماست
 
     const loader = document.getElementById('loader');
     const appContainer = document.getElementById('app-container');

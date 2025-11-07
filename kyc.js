7a41562e29ffec86ae301dee53635d1f446a3fe9
mehdikamali1 <mehdi.kamali2020@gmail.com>
2025-11-08 00:01:03 +0330

    Add files via upload

--- kyc.js
+++ kyc.js
@@ -4,7 +4,7 @@
     // ------------------------------------------------------------------------
     // ⚠️ آدرس Cloudflare شما باید در اینجا صحیح باشد
     // ------------------------------------------------------------------------
-    const API_BASE_URL = "https://motorcycles-luis-tutorials-submissions.trycloudflare.com"; // <-- ❗️❗️❗️ مطمئن شوید این آدرس درست است
+    const API_BASE_URL = "https://innovation-laboratory-courses-euro.trycloudflare.com"; // <-- ❗️❗️❗️ مطمئن شوید این آدرس درست است
 
     // --- عناصر DOM ---
     const formContainer = document.getElementById('kyc-form-container');

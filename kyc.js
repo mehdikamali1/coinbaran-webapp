4b04b084cadb18f56c55160a26cefae880789284
mehdikamali1 <mehdi.kamali2020@gmail.com>
2025-11-10 21:13:56 +0330

    Add files via upload

--- kyc.js
+++ kyc.js
@@ -4,7 +4,7 @@
     // ------------------------------------------------------------------------
     // ⚠️ آدرس Cloudflare شما باید در اینجا صحیح باشد
     // ------------------------------------------------------------------------
-    const API_BASE_URL = "https://dealers-viewed-avon-universal.trycloudflare.com"; // <-- ❗️❗️❗️ مطمئن شوید این آدرس درست است
+    const API_BASE_URL = "https://overcome-bugs-commentary-broader.trycloudflare.com"; // <-- ❗️❗️❗️ مطمئن شوید این آدرس درست است
 
     // --- عناصر DOM ---
     const formContainer = document.getElementById('kyc-form-container');

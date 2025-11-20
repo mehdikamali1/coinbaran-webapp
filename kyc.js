81757e291600be2c01bfa4146a74b310928ee024
mehdikamali1 <mehdi.kamali2020@gmail.com>
2025-11-21 00:57:11 +0330

    Add files via upload

--- kyc.js
+++ kyc.js
@@ -4,7 +4,7 @@
     // ------------------------------------------------------------------------
     // ⚠️ آدرس Cloudflare شما باید در اینجا صحیح باشد
     // ------------------------------------------------------------------------
-    const API_BASE_URL = "loans-products-effects-ministers.trycloudflare.com"; // <-- ❗️❗️❗️ مطمئن شوید این آدرس درست است
+    const API_BASE_URL = "https://loans-products-effects-ministers.trycloudflare.com"; // <-- ❗️❗️❗️ مطمئن شوید این آدرس درست است
 
     // --- عناصر DOM ---
     const formContainer = document.getElementById('kyc-form-container');
